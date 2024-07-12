060646c6dc31de28350e2fe0a1336bbab5c469c9
Aryan <rs.aryant@gmail.com>
2024-07-12 13:43:11 +0000

    Integrated the API with the website with xmlhttp req

--- start.py
+++ start.py
@@ -7,16 +7,8 @@ data=f.read()
 timetable=json.loads(data)
 c_time=strftime("%w/%H:%M/%p",gmtime()).split("/")
 
-period_map=["7:40-8:20","8:20-9:00","9:00-9:40","9:40-10:20","10:20-11:00","11:00-11:40","11:40-12:20","12:20-13:00","13:00-17:40"]
-"""
-if c_time[2]=="PM" :
-    clock=c_time[1].split(":")
-    if int(clock[0])==1:
-        if int(clock[1])>=45:
-            print("Programme not usuable after school hours ")
-    if int(clock[0])>=2:
-        print("Programme not usuable after school hours ")
-"""
+period_map=["7:40-8:20","8:20-9:00","9:00-9:40","9:40-10:20","10:20-11:00","11:00-11:40","11:40-12:20","12:20-13:00","13:00-13:40","13:40-24:00","00:00-7:20"]
+
 faculties=timetable.keys()
 
 def getCurrentPeriod():
@@ -24,19 +16,21 @@ def getCurrentPeriod():
     for timing in period_map:
         limits=timing.split("-")
         p_start=limits[0].split(":")
-        print(f"Checking for period {period}")
-        print(f"start time {p_start}")
+        #print(f"Checking for period {period}")
+        #print(f"start time {p_start}")
         p_end=limits[1].split(":")
-        print(f"end time {p_end}")
-        print(c_time[1].split(":")[0])
-        print(c_time[1].split(":")[1])
+        #print(f"end time {p_end}")
+        #print(c_time[1].split(":")[0])
+        #print(c_time[1].split(":")[1])
         if int(c_time[1].split(":")[0])>=int(p_start[0]) and int(c_time[1].split(":")[0])<=int(p_end[0]):
-            print("True")
+         #   print("True")
             if int(c_time[1].split(":")[0])==int(p_end[0]):
-                print("True")
+          #      print("True")
                 if int(c_time[1].split(":")[1])>=int(p_start[1]) and int(c_time[1].split(":")[1])<=int(p_end[1]):
-                    print("True")
+           #         print("True")
                     return period
+                period+=1
+                continue
             return period
         period+=1
     return 999
@@ -47,25 +41,32 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/search',methods=["GET"])
+@app.route('/search',methods=["POST"])
 def search():
-    if request.method=="GET":
+    if request.method=="POST":
         results=[]
-        query=request.args['query'].lower()
-        print(f"Searching for {query}")
-        for name in faculties:
-            print(f"trying to match with {name}")
-            i=0
-            while len(query)+i<=len(name):
-                print(name[i:len(query)+i].lower())
-                if query==name[i:len(query)+i].lower():
-                    results.append(name)
-                    break
-                i+=1
-        data=dict()
-        for j in results:
-            data[j]=timetable[j][int(c_time[0])][getCurrentPeriod()]
-        return data
+        try:
+            query=request.form['query'].lower()
+            if query == None:
+                return 'Invalid Parameters'
+            #print(f"Searching for {query}")
+            for name in faculties:
+                #print(f"trying to match with {name}")
+                i=0
+                while len(query)+i<=len(name):
+                    #print(name[i:len(query)+i].lower())
+                    if query==name[i:len(query)+i].lower():
+                        results.append(name)
+                        break
+                    i+=1
+            data=dict()
+            for j in results:
+                data[j]=timetable[j][int(c_time[0])][getCurrentPeriod()]
+            res = json.dumps(data)
+            #print(res)
+            return res
+        except:
+            return 'Error Occoured'
 
 
 
--- static/main.js
+++ static/main.js
@@ -1,13 +1,41 @@
+
 q_inp=document.querySelector('#query')
-q_inp.addEventListener('onchange',()=>{
-	val = q_inp.value
-	let data;
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = ()=>{
-		if (this.readState==4 and this.status==200){
-			data=JSON.parse(xhttp.responseText)
-		}
-	}
+content=document.querySelector("#content")
 
+function getData(){
+    setTimeout(()=>{
+        let data;
+	    let xhttp = new XMLHttpRequest();
+	    xhttp.onreadystatechange = (state)=>{
+	        if (state.target.readyState==4 && state.target.status==200){
+	             try{
+		            data=JSON.parse(xhttp.responseText)
+		        }catch{
+		        
+		        }
+		        content.innerHTML="";
+		        for (name in data){
+                    let li = document.createElement('li')
+                    li.setAttribute('class',"list-group-item list-group-item-action d-flex justify-content-between align-items-start")
+                    let d1 = document.createElement('div')
+                    d1.setAttribute('class',"ms-2 me-auto")
+                    let span = document.createElement('span')
+                    span.setAttribute('class',"badge text-bg-primary rounded-pill")
+                    span.innerText=data[name]
+                    d1.innerText=name
+                    li.appendChild(d1)
+                    li.appendChild(span)
+                    content.appendChild(li)
+                }
+	        }   
+	    }
+	    var fdata = new FormData()
+	    let val = q_inp.value
+	    fdata.append('query',val)
+	    xhttp.open("POST","/search",true)
+        xhttp.send(fdata);
+    },500)
+   
+}
+q_inp.addEventListener("input",()=>{ getData()});
 
-})
